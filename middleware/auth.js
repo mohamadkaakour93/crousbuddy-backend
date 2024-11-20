6a2e4a3f5b35eca04dd09fd409683406fc128eaa
Mohamadkaakour93 <mhamadelkaakour@gmail.com>
2024-11-20 23:57:37 +0100

    Fix: auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,30 +1,17 @@
 import jwt from 'jsonwebtoken';
 
 const authMiddleware = (req, res, next) => {
-  // Essayer d'obtenir le token depuis le header 'x-auth-token'
-  let token = req.header('x-auth-token');
-
-  // Si le token n'est pas présent, essayer de le récupérer depuis 'Authorization'
-  if (!token) {
-    const authHeader = req.header('Authorization');
-    if (authHeader && authHeader.startsWith('Bearer ')) {
-      token = authHeader.substring(7); // Supprime 'Bearer ' pour obtenir le token
+    const token = req.header('x-auth-token');
+    if (!token) {
+        return res.status(401).json({ message: 'Token manquant. Accès non autorisé.' });
     }
-  }
-
-  if (!token) {
-    return res.status(401).json({ message: 'Aucun token, autorisation refusée.' });
-  }
-
-  try {
-    const jwtSecret = process.env.JWT_SECRET || 'votre_clé_secrète_par_défaut';
-    const decoded = jwt.verify(token, jwtSecret);
-    req.user = decoded.user;
-    next();
-  } catch (err) {
-    console.error('Erreur lors de la vérification du token :', err);
-    res.status(401).json({ message: 'Token invalide.' });
-  }
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded;
+        next();
+    } catch (error) {
+        res.status(401).json({ message: 'Token invalide.' });
+    }
 };
 
 export default authMiddleware;
