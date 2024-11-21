546bb6a4a933ae3a8bb6f23988578f0d36ff2f2d
Mohamadkaakour93 <mhamadelkaakour@gmail.com>
2024-11-21 14:47:22 +0100

    Fix: correction scrape.js et user.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken';
 import User from '../models/User.js';
 
-export async function authMiddleware(req, res, next) {
+export default async function authMiddleware(req, res, next) {
   const authHeader = req.header('Authorization');
   if (!authHeader || !authHeader.startsWith('Bearer ')) {
     return res.status(401).json({ message: 'Token manquant ou invalide.' });
