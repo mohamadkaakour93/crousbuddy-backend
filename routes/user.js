2a46f6a9e1b5007357af189442e9b3c1c0d3b70d
Mohamadkaakour93 <mhamadelkaakour@gmail.com>
2024-11-21 14:55:53 +0100

    Fix: correction scrape.js et user.js

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,7 @@
 import express from 'express';
-import bcrypt from 'bcryptjs';
 import User from '../models/User.js';
-import { scrapeWebsite } from '../scrape.js';
-import authMiddleware from '../middleware/auth.js';
+import { scrapeWebsite } from '../scrape.js'; 
+import { authMiddleware } from '../middleware/auth.js';
 
 
 const router = express.Router();
