4c01d0cc41b4b17bb2c7481f2672cb80cf8841cf
Mohamadkaakour93 <mhamadelkaakour@gmail.com>
2024-11-21 01:13:12 +0100

    Fix: auth.js

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ import express from 'express';
 import bcrypt from 'bcryptjs';
 import User from '../models/User.js';
 import authMiddleware from '../middleware/auth.js';
+import { addUserToQueue } from '../scrape.js';
 
 const router = express.Router();
 
